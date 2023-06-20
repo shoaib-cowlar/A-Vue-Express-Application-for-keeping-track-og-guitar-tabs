f5411056978f8dea213b0bb7ebbbf3c43aa19476
shoaib naseer <shoaib.naseer.cowlar@gmail.com>
2023-06-20 13:00:31 +0500

    Updated User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,29 +1,40 @@
+const bcrypt = require("bcrypt");
+
+
 module.exports = (sequelize, DataTypes) =>
-  sequelize.define(
-    "User",
-    {
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
+  {
+    const User = sequelize.define(
+      "User",
+      {
+        firstName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lastName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true,
+          },
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
         },
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      tableName: "Users",
-    }
-  );
+      {
+        tableName: "Users",
+      }
+    );
+      User.prototype.comparePassword = function (password) {
+        return bcrypt.compare(password, this.password)
+      }
+
+    return User;
+  
+  }
